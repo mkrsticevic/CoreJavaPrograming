be4e4a67e8ee35e1fdf530b541c44907ba1ae63c
unknown <magdalena.peric.1b@gmail.com>
2024-12-30 16:33:32 +0100

    ovo je nasa inicijalna verzija

---
+++ src/test/java/one/Program.java
@@ -0,0 +1,10 @@
+package one;
+//psvm
+//sout
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Vozdra raja šta ima");
+        System.out.println("Kako ste mi danas?");
+    }
+}
